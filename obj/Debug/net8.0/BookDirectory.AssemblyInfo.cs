a859347f486781ef40722f8bb17869969a84d2bd
Erika Vestin <erbj1201@student.miun.se>
2024-02-15 17:04:34 +0100

    bin

--- obj/Debug/net8.0/BookDirectory.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookDirectory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookDirectory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8157e197b40cbabdbfcdb3e874a57f5c0e7b172f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookDirectory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookDirectory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
